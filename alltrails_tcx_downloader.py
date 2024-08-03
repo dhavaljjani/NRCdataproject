2e6c71b99d80475d990366417fa79364e772f9b9
Dhaval Jani <56317794+dhavaljjani@users.noreply.github.com>
2024-08-03 00:55:02 -0700

    initial script

---
+++ alltrails_tcx_downloader.py
@@ -0,0 +1,26 @@
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+
+running_data_path = r"C:\Users\dhava\Downloads\jobId=bb819631-be31-4271-b4f2-2b49b79460a3\tcx"
+
+cService = webdriver.ChromeService(executable_path=r"C:\Users\dhava\Downloads\chromedriver-win64\chromedriver-win64\chromedriver.exe")
+driver = webdriver.Chrome(service=cService)
+
+file_upload = "FileUpload-module__hidden___leG7z"
+
+for file_name in os.listdir(running_data_path):
+    time.sleep(5)
+    driver.get("https://www.alltrails.com/converter")
+    print(running_data_path + "/" + file_name)
+    time.sleep(5)
+    driver.find_element(By.CLASS_NAME, file_upload).send_keys(running_data_path + "\\" + file_name)
+    #print(driver.find_element(By.CLASS_NAME, filetype_select))
+    time.sleep(5)
+    select = Select(driver.find_element(By.XPATH("/html/body/div[2]/div[4]/div[1]/div[1]/div/form/div/div[2]/div/label/div[2]/select")).get_attribute("innerHTML"))
+    select.select_by_visible_text("Google Earth KML")
+    time.sleep(5)
+    driver.find_element(By.XPATH("/html/body/div[2]/div[4]/div[1]/div[1]/div/form/div/div[4]/button")).get_attribute("innerHTML").click()
+    time.sleep(10)
